04cbdff3cd9666d8eea608dd335469c68030d5e3
Luka Brkovic <coding@lbrkovic.com>
2023-05-10 10:25:14 +0200

    listening query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Container from "@/components/Container";
-import EmptyStaate from "@/components/EmptyState";
+import EmptyState from "@/components/EmptyState";
 import Hydrate from "@/components/Hydrate";
+import ListingCard from "@/components/listings/ListingCard";
+import getCurrentUser from "@/utils/getCurrentUser";
+import getListings from "@/utils/getListings";
 
-export default function Home() {
-  const isEmpty = true ;
-  if (isEmpty) {
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser()
+
+  if (listings.length === 0) {
    return (
     <Hydrate>
-      <EmptyStaate />
+      <EmptyState showReset />
     </Hydrate>
    ) 
   }
@@ -16,7 +22,15 @@ export default function Home() {
     <Hydrate>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-9">
-        <h2>future lisings </h2>
+        {
+          listings.map((listing) => (
+            <ListingCard 
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+            />
+            ))
+        }
         </div>
       </Container>
     </Hydrate>
---
+++ src/components/listings/ListingCard.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react';
+
+interface ListingCardProps {
+  
+}
+
+const ListingCard: FC<ListingCardProps> = ({}) => {
+  return (
+<div> 
+ListingCard
+</div>
+)
+}
+
+export default ListingCard
