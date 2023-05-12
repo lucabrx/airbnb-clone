246c8d7b763990f59c4e40394741e4b4dd89fab4
Luka Brkovic <coding@lbrkovic.com>
2023-05-12 11:13:05 +0200

    listening info and hero

--- src/components/ListingClient.tsx
+++ src/components/ListingClient.tsx
@@ -1,6 +1,10 @@
+"use client"
 import { SafeListing, SafeReservation, SafeUser } from '@/types';
 import { useMemo, type FC } from 'react';
 import { categories } from "@/components/navbar/Categories";
+import Container from './Container';
+import ListingHead from './listings/ListingHead';
+import ListingInfo from './listings/ListingInfo';
 
 interface ListingClientProps {
     reservations?: SafeReservation[];
@@ -18,9 +22,30 @@ const ListingClient: FC<ListingClientProps> = ({listing,currentUser,reservations
      }, [listing.category]);
    
   return (
-<div> 
-ListingClient
+<Container> 
+<div className='max-w-screen-lg mx-auto'>
+    <div className='flex flex-col gap-6'>
+    <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+            <ListingInfo 
+            user={listing.user}
+            category={category}
+            description={listing.description}
+            roomCount={listing.roomCount}
+            guestCount={listing.guestCount}
+            bathroomCount={listing.bathroomCount}
+            locationValue={listing.locationValue}
+            />
+        </div>
+    </div>
 </div>
+</Container>
 )
 }
 
