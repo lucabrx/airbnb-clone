220e945cea9367e243d62fdc1cb7dad79af8b28d
Luka Brkovic <coding@lbrkovic.com>
2023-05-12 10:09:02 +0200

    console error fixes

--- src/components/listings/ListingCard.tsx
+++ src/components/listings/ListingCard.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
 import HeartButton from '../HeartButton';
 import Button from '../Button';
 interface ListingCardProps {
-    data: Listing;
+    data: SafeListing;
     reservation?: Reservation;
     onAction?: (id: string) => void;
     disabled?: boolean;
