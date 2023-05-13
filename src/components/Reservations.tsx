d3783d341dfe11993b1f12af3acf364bceb9b25d
Luka Brkovic <coding@lbrkovic.com>
2023-05-13 08:20:30 +0200

    ui

--- src/components/Reservations.tsx
+++ src/components/Reservations.tsx
@@ -46,17 +46,7 @@ const Reservations: FC<ReservationsClientProps> = ({
         subtitle="Bookings on your properties"
       />
       <div 
-        className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
+        className=" mt-10 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
       >
         {reservations.map((reservation: any) => (
           <ListingCard
