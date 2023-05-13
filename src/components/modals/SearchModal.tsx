4744e06d742952ed2f755c50efe5ac8b73818fbb
Luka Brkovic <coding@lbrkovic.com>
2023-05-13 13:59:13 +0200

    search modal

---
+++ src/components/modals/SearchModal.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react';
+
+interface SearchModalProps {
+  
+}
+
+const SearchModal: FC<SearchModalProps> = ({}) => {
+  return (
+<div> 
+SearchModal
+</div>
+)
+}
+
+export default SearchModal
