8c94b113f83821ad83cd434b98d45fc1390a6075
Luka Brkovic <coding@lbrkovic.com>
2023-05-13 14:35:22 +0200

    loading screen

---
+++ src/components/Loader.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ClipLoader } from "react-spinners";
+
+const Loader = () => {
+  return ( 
+    <div
+    className="
+      h-[70vh]
+      flex 
+      flex-col 
+      justify-center 
+      items-center 
+    "
+    >
+      <ClipLoader
+        size={100}
+        color="red"
+      />
+    </div>
+   );
+}
+ 
+export default Loader;
